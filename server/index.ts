226bc948ba612492eb9f12e717911d11275952e3
roynx98 <roynx98@gmail.com>
2025-10-29 11:59:38 -0700

    feat: Setup dotenv

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import "./config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -75,7 +76,7 @@ app.use((req, res, next) => {
   server.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
+    // reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
   });
